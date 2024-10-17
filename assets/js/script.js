081711ced03977f015c7397470ff23a12b46fe90
breemwaniki93 <bridgettemwaniki@gmail.com>
2024-10-17 09:17:09 +0300

    intial commit

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+const colorButton=document.getElementById(elementid: 'colorButton');
+
+colorButton.addEventListener('click',():void=>{
+    colorButton.style.backgroundColor=colorButton.style.backgroundColor=== 'blue'?'#4CAF50':'blue'
+
+}
